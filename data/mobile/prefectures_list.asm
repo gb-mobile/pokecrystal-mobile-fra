9bc075ff910f3589879f612cf1286e8cdf0bc23d
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-04-15 02:21:51 -0400

    Update prefectures_list.asm

--- data/mobile/prefectures_list.asm
+++ data/mobile/prefectures_list.asm
@@ -62,7 +62,6 @@ Prefectures: ; Some names shortened to fit, check for official initials later. I
 	db " LU-WI@"      ; Wiltz
 
 PrefecturesScrollList: ; Some names shortened to fit, check for official initials later. In this list, strings must be right-aligned.
-Prefectures: ; Some names shortened to fit, check for official initials later. In this list, strings must be right-aligned.
 	db "FR-ARA@"      ; Auvergne-Rhône-Alpes
 	db "FR-BFC@"      ; Bourgogne-Franche-Comté
 	db "FR-BRE@"      ; Bretagne
@@ -86,105 +85,42 @@ Prefectures: ; Some names shortened to fit, check for official initials later. I
 	db "BE-WLG@"      ; Liège
 	db "BE-WLX@"      ; Luxembourg
 	db "BE-WNA@"      ; Namur
-	db " CH-AG@"      ; Aargau
-	db " CH-AI@"      ; Appenzell Innerrhoden
-	db " CH-AR@"      ; Appenzell Ausserrhoden
-	db " CH-BE@"      ; Berne
-	db " CH-BL@"      ; Basel-Landschaft
-	db " CH-BS@"      ; Basel-Stadt
-	db " CH-FR@"      ; Fribourg
-	db " CH-GE@"      ; Genève
-	db " CH-GL@"      ; Glarus
-	db " CH-GR@"      ; Graubünden
-	db " CH-JU@"      ; Jura
-	db " CH-LU@"      ; Luzer
-	db " CH-NE@"      ; Neuchâtel
-	db " CH-NW@"      ; Nidwalden
-	db " CH-OW@"      ; Obwalden
-	db " CH-SG@"      ; Sankt Gallen
-	db " CH-SH@"      ; Schaffhausen
-	db " CH-SO@"      ; Solothurn
-	db " CH-SZ@"      ; Schwyz
-	db " CH-TG@"      ; Thurgau
-	db " CH-TI@"      ; Ticino
-	db " CH-UR@"      ; Uri
-	db " CH-VD@"      ; Vaud
-	db " CH-VS@"      ; Valais
-	db " CH-ZG@"      ; Zug
-	db " CH-ZH@"      ; Zürich
-	db " LU-CA@"      ; Capellen
-	db " LU-CL@"      ; Clervaux
-	db " LU-DI@"      ; Diekirch
-	db " LU-EC@"      ; Echternach
-	db " LU-ES@"      ; Esch-sur-Alzette
-	db " LU-GR@"      ; Grevenmacher
-	db " LU-LU@"      ; Luxembourg
-	db " LU-ME@"      ; Mersch
-	db " LU-RD@"      ; Redange
-	db " LU-RM@"      ; Remich
-	db " LU-VD@"      ; Vianden
-	db " LU-WI@"      ; Wiltz
-
-PrefecturesScrollList: ; Some names shortened to fit, check for official initials later. In this list, strings must be right-aligned.
-	db "FR-ARA@"      ; Auvergne-Rhône-Alpes
-	db "FR-BFC@"      ; Bourgogne-Franche-Comté
-	db "FR-BRE@"      ; Bretagne
-	db "FR-CVL@"      ; Centre-Val de Loire
-	db "FR-20R@"      ; Corse	
-	db "FR-GES@"      ; Grand-Est
-	db "FR-HDF@"      ; Hauts-de-France
-	db "FR-IDF@"      ; Île-de-France
-	db "FR-NAQ@"      ; Nouvelle-Aquitaine
-	db "FR-NOR@"      ; Normandie
-	db "FR-OCC@"      ; Occitanie
-	db "FR-PAC@"      ; Provence-Alpes-Côte-d’Azur
-	db "FR-PDL@"      ; Pays-de-la-Loire
-	db "BE-VAN@"      ; Antwerpen
-	db "BE-VBR@"      ; Vlaams-Brabant
-	db "BE-VLI@"      ; Limburg
-	db "BE-VOV@"      ; Oost-Vlaanderen
-	db "BE-VWV@"      ; West-Vlaanderen
-	db "BE-WBR@"      ; Brabant wallon
-	db "BE-WHT@"      ; Hainaut
-	db "BE-WLG@"      ; Liège
-	db "BE-WLX@"      ; Luxembourg
-	db "BE-WNA@"      ; Namur
-	db " CH-AG@"      ; Aargau
-	db " CH-AI@"      ; Appenzell Innerrhoden
-	db " CH-AR@"      ; Appenzell Ausserrhoden
-	db " CH-BE@"      ; Berne
-	db " CH-BL@"      ; Basel-Landschaft
-	db " CH-BS@"      ; Basel-Stadt
-	db " CH-FR@"      ; Fribourg
-	db " CH-GE@"      ; Genève
-	db " CH-GL@"      ; Glarus
-	db " CH-GR@"      ; Graubünden
-	db " CH-JU@"      ; Jura
-	db " CH-LU@"      ; Luzer
-	db " CH-NE@"      ; Neuchâtel
-	db " CH-NW@"      ; Nidwalden
-	db " CH-OW@"      ; Obwalden
-	db " CH-SG@"      ; Sankt Gallen
-	db " CH-SH@"      ; Schaffhausen
-	db " CH-SO@"      ; Solothurn
-	db " CH-SZ@"      ; Schwyz
-	db " CH-TG@"      ; Thurgau
-	db " CH-TI@"      ; Ticino
-	db " CH-UR@"      ; Uri
-	db " CH-VD@"      ; Vaud
-	db " CH-VS@"      ; Valais
-	db " CH-ZG@"      ; Zug
-	db " CH-ZH@"      ; Zürich
-	db " LU-CA@"      ; Capellen
-	db " LU-CL@"      ; Clervaux
-	db " LU-DI@"      ; Diekirch
-	db " LU-EC@"      ; Echternach
-	db " LU-ES@"      ; Esch-sur-Alzette
-	db " LU-GR@"      ; Grevenmacher
-	db " LU-LU@"      ; Luxembourg
-	db " LU-ME@"      ; Mersch
-	db " LU-RD@"      ; Redange
-	db " LU-RM@"      ; Remich
-	db " LU-VD@"      ; Vianden
+	db "CH-AG@"      ; Aargau
+	db "CH-AI@"      ; Appenzell Innerrhoden
+	db "CH-AR@"      ; Appenzell Ausserrhoden
+	db "CH-BE@"      ; Berne
+	db "CH-BL@"      ; Basel-Landschaft
+	db "CH-BS@"      ; Basel-Stadt
+	db "CH-FR@"      ; Fribourg
+	db "CH-GE@"      ; Genève
+	db "CH-GL@"      ; Glarus
+	db "CH-GR@"      ; Graubünden
+	db "CH-JU@"      ; Jura
+	db "CH-LU@"      ; Luzer
+	db "CH-NE@"      ; Neuchâtel
+	db "CH-NW@"      ; Nidwalden
+	db "CH-OW@"      ; Obwalden
+	db "CH-SG@"      ; Sankt Gallen
+	db "CH-SH@"      ; Schaffhausen
+	db "CH-SO@"      ; Solothurn
+	db "CH-SZ@"      ; Schwyz
+	db "CH-TG@"      ; Thurgau
+	db "CH-TI@"      ; Ticino
+	db "CH-UR@"      ; Uri
+	db "CH-VD@"      ; Vaud
+	db "CH-VS@"      ; Valais
+	db "CH-ZG@"      ; Zug
+	db "CH-ZH@"      ; Zürich
+	db "LU-CA@"      ; Capellen
+	db "LU-CL@"      ; Clervaux
+	db "LU-DI@"      ; Diekirch
+	db "LU-EC@"      ; Echternach
+	db "LU-ES@"      ; Esch-sur-Alzette
+	db "LU-GR@"      ; Grevenmacher
+	db "LU-LU@"      ; Luxembourg
+	db "LU-ME@"      ; Mersch
+	db "LU-RD@"      ; Redange
+	db "LU-RM@"      ; Remich
+	db "LU-VD@"      ; Vianden
 LastPrefecture:	db " LU-WI@"      ; Wiltz
 
